4c32a46122a3c32b6beaf8a000b95eb2fcc34783
hardikdabral2003 <hardikdabral2@gmail.com>
2025-05-18 10:51:15 +0530

    added

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -14,7 +14,7 @@ export const registerUser = async (req, res) => {
 
     // const salt = await bcrypt.genSalt(10)
     // const hashedPassword = await bcrypt.hash(password, salt)
-
+    console.log('Request body:', req.body)
     const user = await User.create({
       name,
       email,
@@ -57,7 +57,7 @@ export const loginUser = async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
-      token: generateToken(user._id),  // <-- add token here
+      token: generateToken(user._id),
     })
   } catch (error) {
     console.error('Login error:', error) // Debug log
